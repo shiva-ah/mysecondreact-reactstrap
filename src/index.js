e0fc082a18c93cc7f49dd41c1272130dc9d0be90
shiva-ah <johndoe@example.com>
2021-08-13 22:15:55 +0430

    Your awesome message

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import {} from 'reactstrap';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Page1 from './page1';
+
+/*class Header extends React.Component{
+  render(){
+    return(
+      <div className="row">
+        <div className="col-sm-6" style={{border:"1px solid red",boxSizing:"border-box"}}>one</div>
+        <div className="col-sm-6" style={{border:"1px solid red",boxSizing:"border-box"}}><button className="btn btn-primary">Click</button></div>
+        
+      </div>
+    )
+  }
+}*/
+
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+
+const Example = () => {
+
+		return(
+		<>
+		<BrowserRouter>
+        <Route path="/page1"  component={Page1}/ >
+</BrowserRouter>
+		
+  <div>
+    <Button color="primary" id="toggler" style={{ marginBottom: '1rem' }}>
+      Toggle
+    </Button>
+    <UncontrolledCollapse toggler="#toggler">
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
+          similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
+          dignissimos esse fuga! Minus, alias.
+		  <img src={"../public/logo192.png"}/>
+        </CardBody>
+      </Card>
+    </UncontrolledCollapse>
+  </div>
+  </>
+  )
+};
+ReactDOM.render(<Example/>,document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
---
+++ src/page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Page1() {
+  return (
+    <div>
+ pag1
+    </div>
+  );
+}
+
+export default Page1;
